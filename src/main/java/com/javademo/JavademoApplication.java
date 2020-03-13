24aefe3b0d85d57e9e9f78de0db76bc0ef8223f7
13375 <1337515006@qq.com>
2020-03-13 17:59:14 +0800

    3.13  第一个demo工程

---
+++ src/main/java/com/javademo/JavademoApplication.java
@@ -0,0 +1,13 @@
+package com.javademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavademoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/javademo/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.javademo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by 13375 on 2020/3/13.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("").setViewName("uploadhtml");
+    }
+}
